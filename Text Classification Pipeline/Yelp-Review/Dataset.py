8c72a3eaca4b9310c3316ff2696bfb7216b866be
srishti-git1110 <84127491+srishti-git1110@users.noreply.github.com>
2022-09-15 15:39:45 +0530

    Update Dataset.py

--- Text Classification Pipeline/Yelp-Review/Dataset.py
+++ Text Classification Pipeline/Yelp-Review/Dataset.py
@@ -6,13 +6,13 @@ class YelpReview(Dataset):
         self.df = review_df
         self._vectorizer = vectorizer
         
-        self.train_data = self.df[self.df['split']=='train']
+        self.train_df = self.df[self.df['split']=='train']
         self.train_size = len(self.train_data)
         
-        self.val_data = self.df[self.df['split']=='validation']
+        self.val_df = self.df[self.df['split']=='validation']
         self.val_size = len(self.val_data)
         
-        self.test_data = self.df[self.df['split']=='test']
+        self.test_df = self.df[self.df['split']=='test']
         self.test_size = len(self.test_data)
         
         self._lookup_dict = {'train': (self.train_df, self.train_size),
@@ -35,11 +35,14 @@ class YelpReview(Dataset):
     def __len__():
         return self._target_size
     
-    def __getitem__(self, index):
-        label = review_df.iloc[index, 'class']  # need to change
-        review = self._vectorizer.vectorize(review_df.iloc[index, 'review'])
+    def __getitem__(self, idx):
+        row = self._target_df.iloc[idx, :] # need to change
+        label = row['class']
+        review = self._vectorizer.vectorize(row['review'])
         return {'x': review,
                 'y': label} 
     
     def num_of_batches(self, batch_size):
         return len(self) // batch_size
+
+   
